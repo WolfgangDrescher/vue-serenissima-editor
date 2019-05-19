eab31fc4d111aaa40ae1d31df159504cf0ed7d32
Wolfgang Drescher <drescher.wolfgang+git@gmail.com>
2019-05-19 23:55:16 +0200

    Added key component and gui elements

--- src/components/Gui.vue
+++ src/components/Gui.vue
@@ -1,3 +1,106 @@
 <template>
-    <div class="vse__gui"></div>
+    <div class="vse__gui">
+        <template v-for="keyCode in filteredKeyCodes">
+            <key :key="keyCode.keyCode" :key-code="keyCode.keyCode" />
+            <template v-for="variant in keyCode.variants">
+                <key :key="keyCode.keyCode + variant" :key-code="keyCode.keyCode + variant" />
+            </template>
+        </template>
+    </div>
 </template>
+
+<script>
+import Key from './Key.vue';
+
+export default {
+    components: { Key },
+    data() {
+        return {
+            keyCodes: [
+                {
+                    keyCode: 'C',
+                    variants: ['1' ,'2', '3', '4'],
+                },
+                {
+                    keyCode: 'F',
+                    variants: ['4' ,'3'],
+                },
+                {
+                    keyCode: 'G',
+                    variants: ['2' ,'1'],
+                },
+                {
+                    keyCode: 'T',
+                    variants: ['1' ,'2', '3', '4', '5', '6', '7', '8', '9'],
+                },
+                ...this.getForAllLines([
+                    '1' ,'2', '3', '4', '5', '6', '7', '8', 'q', 'w', 'e',
+                ]),
+                ...this.getForAllLines([
+                    '10' ,'20', '30', '40', '50', '60', '70', /*'80',*/ 'q0', 'w0', 'e0',
+                ]),
+                ...this.getForAllLines([
+                    '+', '-', 'U', '.',
+                ]),
+                ...this.getSpecialCharacters([
+                    'u', 'i', 'o', 'p', 'B', 'E', 'R',
+                ]),
+            ],
+        };
+    },
+    computed: {
+        lastScoreCharacter() {
+            return this.$store.state.score.substr(-1);
+        },
+        filteredKeyCodes() {
+            let result = this.keyCodes.filter(keyCode => {
+                return this.lastScoreCharacter === keyCode.keyCode || (keyCode.variants.includes(this.lastScoreCharacter) === true);
+            });
+            result.map(keyCode => {
+                if(keyCode.keyCode !== this.lastScoreCharacter) {
+                    keyCode.variants = [this.lastScoreCharacter];
+                }
+            });
+            return result.length ? result : this.keyCodes;
+        },
+    },
+    methods: {
+        getForAllLines(symbols) {
+            let keyCodes = [];
+            symbols.forEach((symbol) => {
+                keyCodes.push({
+                    keyCode: symbol,
+                    variants: ['z', 's', 'x', 'd', 'c', 'f', 'v', 'g', 'b', 'h', 'n', 'j', 'm', 'k', ','],
+                });
+            });
+            return keyCodes;
+        },
+        getSpecialCharacters(symbols) {
+            let keyCodes = [];
+            symbols.forEach((symbol) => {
+                keyCodes.push({
+                    keyCode: symbol,
+                    variants: [],
+                });
+            });
+            return keyCodes;
+        },
+    },
+};
+</script>
+
+<style scoped lang="scss">
+    .vse__gui {
+        display: flex;
+        flex-wrap: nowrap;
+        overflow-x: auto;
+
+        > * {
+            flex: 0 0 auto;
+        }
+
+        &::-webkit-scrollbar {
+            display: none;
+        }
+    }
+</style>
---
+++ src/components/Key.vue
@@ -0,0 +1,59 @@
+<template>
+    <div class="vse__key" @click="add">
+        <div class="vse__preview">{{ keyCode }}</div>
+        <kbd>{{ keyCode }}</kbd>
+    </div>
+</template>
+
+<script>
+export default {
+    props: {
+        keyCode: {
+            type: String,
+            required: true,
+        },
+    },
+    methods: {
+        add() {
+            this.$store.commit('setScore', this.$store.state.score + this.keyCode);
+        },
+    },
+};
+</script>
+
+<style scoped lang="scss">
+    .vse__key {
+        text-align: center;
+        margin: 0 5px;
+
+        kbd {
+            width: 25px;
+            height: 25px;
+            border: 1px solid #a9a9a9;
+            border-radius: 2px;
+            font-size: 12px;
+            color: #7c7c7c;
+            cursor: pointer;
+            display: flex;
+            justify-content: center;
+            flex-direction: column;
+            margin: 0 auto;
+            overflow: visible;
+        }
+
+        kbd:hover {
+            background-color: #75b4fb;
+            border: 1px solid #5a88c5;
+            color: #fff;
+            text-shadow: 1px 1px 1px rgba(0,0,0,0.2);
+        }
+    }
+
+    .vse__preview {
+        font-family: 'Serenissima';
+        font-size: 4rem;
+        display: flex;
+        justify-content: center;
+        flex-direction: column;
+    }
+</style>
