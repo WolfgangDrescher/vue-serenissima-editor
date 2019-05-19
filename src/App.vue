8289bcadc88f2f4496640230820d4caeabcd21ef
Wolfgang Drescher <drescher.wolfgang+git@gmail.com>
2019-05-19 12:47:52 +0200

    Added basic scss setup

--- src/App.vue
+++ src/App.vue
@@ -13,12 +13,14 @@ export default {
 </script>
 
 <style scoped lang="scss">
+    @import './assets/scss/import.scss';
+
     .vse__app {
+        width: 100%;
+        height: 100%;
         font-family: 'Avenir', Helvetica, Arial, sans-serif;
         -webkit-font-smoothing: antialiased;
         -moz-osx-font-smoothing: grayscale;
-        text-align: center;
-        color: #2c3e50;
-        margin-top: 60px;
+        color: $textColor;
     }
 </style>
