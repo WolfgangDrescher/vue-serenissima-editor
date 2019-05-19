57203cd737eda2251e7c1443003194dca6af515f
Wolfgang Drescher <drescher.wolfgang+git@gmail.com>
2019-05-19 18:13:37 +0200

    Basic implementation to write music

--- src/views/editor.vue
+++ src/views/editor.vue
@@ -1,3 +1,45 @@
 <template>
-    <h1>vue-serenissima-editor</h1>
+    <div>
+        <div class="vse__score">{{ score }}</div>
+        <div class="vse__input">
+            <input type="text" v-model="score"/>
+        </div>
+    </div>
 </template>
+
+<script>
+export default {
+    data() {
+        return {
+            score: '',
+        };
+    },
+    methods: {
+
+    },
+};
+</script>
+
+<style scoped lang="scss">
+    @font-face {
+        font-family: Serenissima;
+        src: url("../assets/EMS_Serenissima.otf") format("opentype");
+    }
+    .vse__score {
+        font-family: 'Serenissima';
+        font-size: 5rem;
+        font-variant-ligatures: normal;
+        word-break: break-all;
+    }
+    .vse__input {
+        position: absolute;
+        bottom: 0;
+        width: 100%;
+        padding: 15px;
+    }
+    .vse__input input {
+        font-size: 2rem;
+        font-family: monospace;
+        width: 100%;
+    }
+</style>
