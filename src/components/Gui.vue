a586b43c7c78c47d832b45872b5c03f5e5cf4a36
Wolfgang Drescher <drescher.wolfgang+git@gmail.com>
2019-05-22 22:12:25 +0200

    Add a rest to filtered results

--- src/components/Gui.vue
+++ src/components/Gui.vue
@@ -88,7 +88,7 @@ export default {
             if(lastChar.length === 1) {
                 results = this.allKeyCodes.filter((value) => {
                     if(lastChar === value) return false;
-                    return value.startsWith(lastChar) && value[1] !== '0';
+                    return value.startsWith(lastChar) && (value[1] !== '0') || (value === lastChar + '0');
                 });
                 if(results.length) {
                     return results;
