392df56237a5b8f407f0243188fbaf5832efe1cc
Wolfgang Drescher <drescher.wolfgang+git@gmail.com>
2019-05-31 20:51:40 +0200

    Added layout with sidebar

---
+++ src/components/Sidebar.vue
@@ -0,0 +1,12 @@
+<template>
+    <aside>
+        <h1>vue-serenissima-editor</h1>
+    </aside>
+</template>
+
+<style scoped lang="scss">
+    aside {
+        display: block;
+        background-color: rgba(0,0,0,.05);
+    }
+</style>
--- src/views/editor.vue
+++ src/views/editor.vue
@@ -1,10 +1,17 @@
 <template>
     <div class="vse__editor">
-        <component is="ScoreCanvas" :score="$store.state.score"></component>
-        <div class="vse__footer">
-            <console />
-            <gui class="vse__gui" />
+        <div class="vse__main-wrapper">
+            <div class="vse__main">
+                <div class="vse__score-wrapper">
+                    <component is="ScoreFont" :score="$store.state.score" />
+                </div>
+                <div class="vse__footer">
+                    <console />
+                    <gui class="vse__gui" />
+                </div>
+            </div>
         </div>
+        <sidebar class="vse__aside" />
     </div>
 </template>
 
@@ -13,9 +20,10 @@ import ScoreFont from '../components/ScoreFont.vue';
 import ScoreCanvas from '../components/ScoreCanvas.vue';
 import Console from '../components/Console.vue';
 import Gui from '../components/Gui.vue';
+import Sidebar from '../components/Sidebar.vue';
 
 export default {
-    components: { ScoreFont, ScoreCanvas, Console, Gui },
+    components: { ScoreFont, ScoreCanvas, Console, Gui, Sidebar },
 };
 </script>
 
@@ -28,13 +36,32 @@ export default {
         height: 100%;
     }
     .vse__footer {
-        position: absolute;
-        bottom: 0;
-        width: 100%;
         padding: 1rem;
-        background-color: $footerColor;
+        border-top: 1px solid $borderColor;
     }
     .vse__gui {
         margin-top: 1rem;
+    }
+    .vse__main-wrapper {
+        height: 100%;
+        padding-right: $sidebarWidth;
+    }
+    .vse__main {
+        position: relative;
+        height: 100%;
+        display: flex;
+        flex-grow: 1;
+        flex-direction: column;
+    }
+    .vse__score-wrapper {
+        flex-grow: 1;
+        overflow-y: auto;
+    }
+    .vse__aside {
+        position: absolute;
+        width: $sidebarWidth;
+        height: 100%;
+        right: 0;
+        top: 0;
     }
 </style>
