94a4bc8b4e61abeb5b8b7f50d53138a68f335db5
Wolfgang Drescher <drescher.wolfgang+git@gmail.com>
2019-06-03 21:33:44 +0200

    Move canvas methods into a plugin file

--- src/components/ScoreCanvas.vue
+++ src/components/ScoreCanvas.vue
@@ -5,6 +5,8 @@
 </template>
 
 <script>
+import { renderCanvas } from '../score-canvas-methods.js';
+
 export default {
     props: {
         score: {
@@ -19,73 +21,19 @@ export default {
     },
     watch: {
         score() {
-            this.renderCanvas();
+            renderCanvas(this.$refs.scoreCanvas, this.score);
         },
     },
     mounted() {
+        renderCanvas(this.$refs.scoreCanvas, this.score, this.$el.clientWidth, this.$el.clientHeight);
         window.addEventListener('resize', this.onResize);
-        this.renderCanvas();
     },
     destroyed() {
         window.removeEventListener('resize', this.onResize);
     },
     methods: {
-        renderCanvas() {
-            this.context = this.$refs.scoreCanvas.getContext('2d');
-            this.setCanvasSizes();
-            this.clearCanvas();
-            this.context.font = '5rem Serenissima';
-            this.context.textBaseline = 'top';
-            this.context.fillStyle = '#2c3e50';
-            this.fillTextMultiline(this.context, this.score, 0, 0, this.measureTextHeight(), this.context.canvas.clientWidth);
-        },
-        clearCanvas() {
-            let ratio = this.pixelRatio();
-            this.context.clearRect(0, 0, this.context.canvas.width, this.context.canvas.height);
-            this.context.setTransform(ratio, 0, 0, ratio, 0, 0);
-        },
         onResize() {
-            this.renderCanvas();
-        },
-        setCanvasSizes() {
-            let ratio = this.pixelRatio(),
-                width = this.$el.clientWidth,
-                height = this.$el.clientHeight;
-            this.context.canvas.width = width * ratio;
-            this.context.canvas.height = height * ratio;
-            this.context.canvas.style.width = width + 'px';
-            this.context.canvas.style.height = height + 'px';
-        },
-        pixelRatio() {
-            let dpr = window.devicePixelRatio || 1,
-                bsr = this.context.webkitBackingStorePixelRatio ||
-                    this.context.mozBackingStorePixelRatio ||
-                    this.context.msBackingStorePixelRatio ||
-                    this.context.oBackingStorePixelRatio ||
-                    this.context.backingStorePixelRatio || 1;
-            return dpr / bsr;
-        },
-        fillTextMultiline(context, text, x, y, lineHeight, fitWidth) {
-            // https://stackoverflow.com/a/4478894/9009012
-            // https://codepen.io/bramus/pen/eZYqoO
-            fitWidth = fitWidth || 0;
-            if (fitWidth <= 0) {
-                context.fillText(text, x, y);
-                return;
-            }
-            for (let idx = 1; idx <= text.length; idx++) {
-                let str = text.substr(0, idx);
-                if(context.measureText(str).width > fitWidth) {
-                    context.fillText(text.substr(0, idx-1), x, y);
-                    this.fillTextMultiline(context, text.substr(idx-1), x, y + lineHeight, lineHeight,  fitWidth);
-                    return;
-                }
-            }
-            context.fillText(text, x, y);
-        },
-        measureTextHeight() {
-            const ratio = 10/49;
-            return this.context.measureText('C3').width / ratio;
+            renderCanvas(this.$refs.scoreCanvas, this.score, this.$el.clientWidth, this.$el.clientHeight);
         },
     },
 };
---
+++ src/score-canvas-methods.js
@@ -0,0 +1,59 @@
+export const renderCanvas = function(canvas, text, width, height) {
+    const context = canvas.getContext('2d');
+    width = width || canvas.clientWidth;
+    height = height || canvas.clientHeight;
+    setCanvasSizes(context, width, height);
+    clearCanvas(context);
+    context.font = '5rem Serenissima';
+    context.textBaseline = 'top';
+    context.fillStyle = '#2c3e50';
+    fillTextMultiline(context, text, 0, 0, measureTextHeight(context), width);
+};
+
+export const clearCanvas = function(context) {
+    let ratio = pixelRatio(context);
+    context.clearRect(0, 0, context.canvas.width, context.canvas.height);
+    context.setTransform(ratio, 0, 0, ratio, 0, 0);
+};
+
+export const setCanvasSizes = function(context, width, height) {
+    let ratio = pixelRatio(context);
+    context.canvas.width = width * ratio;
+    context.canvas.height = height * ratio;
+    context.canvas.style.width = width + 'px';
+    context.canvas.style.height = height + 'px';
+};
+
+export const pixelRatio = function(context) {
+    let dpr = window.devicePixelRatio || 1,
+        bsr = context.webkitBackingStorePixelRatio ||
+            context.mozBackingStorePixelRatio ||
+            context.msBackingStorePixelRatio ||
+            context.oBackingStorePixelRatio ||
+            context.backingStorePixelRatio || 1;
+    return dpr / bsr;
+};
+
+export const fillTextMultiline = function(context, text, x, y, lineHeight, fitWidth) {
+    // https://stackoverflow.com/a/4478894/9009012
+    // https://codepen.io/bramus/pen/eZYqoO
+    fitWidth = fitWidth || 0;
+    if (fitWidth <= 0) {
+        context.fillText(text, x, y);
+        return;
+    }
+    for (let idx = 1; idx <= text.length; idx++) {
+        let str = text.substr(0, idx);
+        if(context.measureText(str).width > fitWidth) {
+            context.fillText(text.substr(0, idx-1), x, y);
+            fillTextMultiline(context, text.substr(idx-1), x, y + lineHeight, lineHeight,  fitWidth);
+            return;
+        }
+    }
+    context.fillText(text, x, y);
+};
+
+export const measureTextHeight = function(context) {
+    const ratio = 10/49;
+    return context.measureText('C3').width / ratio;
+};
